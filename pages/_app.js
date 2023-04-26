cacf343de2c0d861114ccfb2a13253ed97aafd55
HelenBassa <helenbassa@gmail.com>
2023-04-26 23:44:48 +0300

    add Roboto Slab font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Roboto_Slab } from "next/font/google";
+
+const robotoSlab = Roboto_Slab({
+  weight: "400",
+  subsets: ["latin", "cyrillic"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={robotoSlab.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
